4f628db3c22e6097a98491eee259c478b0225e04
Brin29 <breinerstivenparracortes50@gmail.com>
2025-05-06 14:09:05 -0500

    first commit

---
+++ src/main/java/brien/pets_name/PetsNameAiApplication.java
@@ -0,0 +1,13 @@
+package brien.pets_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsNameAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsNameAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brien/pets_name/PetsNameAiApplicationTests.java
@@ -0,0 +1,13 @@
+package brien.pets_name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsNameAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
