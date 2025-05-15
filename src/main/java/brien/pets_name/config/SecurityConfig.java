1cdfaa6db026e05268ed7955226643d6c6aae433
Brin29 <breinerstivenparracortes50@gmail.com>
2025-05-15 08:48:49 -0500

    cambios

---
+++ src/main/java/brien/pets_name/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package brien.pets_name.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+
+
+        return  httpSecurity.build();
+
+
+    }
+}
